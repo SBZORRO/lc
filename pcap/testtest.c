786b9218ceb9cd23faa2a9fd24a8a7eb5a31952c
SBZORRO <sbzorro@gmail.com>
2025-02-05 17:54:54 +0800

    wip

--- pcap/testtest.c
+++ pcap/testtest.c
@@ -1,4 +1,8 @@
+#include <arpa/inet.h>
+#include <netinet/in.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "captotcp.h"
 
 static int main_ret = 0;
@@ -23,48 +27,48 @@ static int test_pass = 0;
 #define EXPECT_EQ_INT(expect, actual) \
   EXPECT_EQ_BASE ((expect) == (actual), expect, actual, "%d")
 
-char *argv[] = { "./hello",
-                 "'(src host 172.17.80.1 and src port 9997) or (dst host "
-                 "172.17.81.3 and dst port 9999)'",
-                 "192.168.5.17:9999", "172.17.81.3:9998" };
-int argc = 4;
+void *
+check_malloc (size_t size)
+{
+  void *ptr;
+
+  if ((ptr = malloc (size)) == NULL)
+    {
+      /* DEBUG(0) ("Malloc failed - out of memory?"); */
+      exit (1);
+    }
+  return ptr;
+}
 
 void
 test_create_flow ()
 {
-  printf ("print 1");
+  char *argv[] = { "./hello",
+                   "'(src host 172.17.80.1 and src port 9997) or (dst host "
+                   "172.17.81.3 and dst port 9999)'",
+                   "192.168.5.17:9999", "172.17.81.3:9998" };
+  int argc = 4;
 
-  flow_t *ptr;
-  int len = 0;
+  char *addr = argv[3];
+  printf ("%s", addr);
+  char *split = strstr (addr, ":");
+  char *port = split + 1;
+  printf ("indi>%s<", ++split);
+  printf ("\ngap: %d", (int) (addr - split));
+  char ip[split - addr - 1];
+  memcpy (ip, addr, split - addr - 1);
+  printf ("\nip:: %s", ip);
+  printf ("\nport:: %d", (int) strlen(port));
 
-  init_flow (ptr, len, argc, argv);
+  /* char* ip = strncpy() */
+  /* char *port = strtok (NULL, "\0"); */
 
-  EXPECT_EQ_INT (1, len);
-}
-
-void
-test_flow_count ()
-{
-}
-void
-test_create_flow_state ()
-{
-}
-
-void
-test_flow_state_count ()
-{
-}
-
-void
-test_flow_state ()
-{
+  /* EXPECT_EQ_INT (1, len); */
 }
 
 int
 main (int argc, char *argv[])
 {
-  printf ("print 1");
 
   test_create_flow ();
   printf ("%d/%d (%3.2f%%) passed\n", test_pass, test_count,
